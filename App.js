080599f2e0550c5067ef2a1c0fbb2b1c7335d3b4
kepin17 <servicenya4@gmail.com>
2022-05-12 14:59:56 +0700

    header section finished

---
+++ App.js
@@ -0,0 +1,42 @@
+const layer = document.getElementById("layer");
+const layer2 = document.getElementById("layer2");
+const layer3 = document.getElementById("layer3");
+
+function layerAnimationIn() {
+  layer.style.top = "60%";
+  layer.style.opacity = ".7";
+}
+function layerAnimationIn2() {
+  layer2.style.top = "60%";
+  layer2.style.opacity = ".7";
+}
+function layerAnimationIn3() {
+  layer3.style.top = "60%";
+  layer3.style.opacity = ".7";
+}
+
+function layerAnimationOut() {
+  layer.style.top = "100%";
+  layer.style.opacity = "1";
+}
+
+function layerAnimationOut2() {
+  layer2.style.top = "100%";
+  layer2.style.opacity = "1";
+}
+
+function layerAnimationOut3() {
+  layer3.style.top = "100%";
+  layer3.style.opacity = "1";
+}
+
+// Profile User
+
+function profileUser() {
+  const profile = document.getElementById("profile");
+  if (profile.style.display === "block") {
+    profile.style.display = "none";
+  } else {
+    profile.style.display = "block";
+  }
+}
